54db2b33b4e85f6d0ac5a92e58f13b96454cbfc9
Oan <oanxtornado@gmail.com>
2020-03-19 01:34:00 +0100

    First Commit

---
+++ Assets/Script/Camera/C_ToPlane.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_ToPlane : MonoBehaviour
+{
+    public Transform pivot;
+    public float cameraDistance = 10f;
+    public float sensitivity = 10f;
+    private float mouseX;
+    private float mouseY;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton (1))
+        {
+            mouseX += Input.GetAxis("Mouse X") * sensitivity;
+            mouseY += Input.GetAxis("Mouse Y") * sensitivity;
+
+            mouseX %= 360;
+            mouseY = Mathf.Clamp(mouseY, -90, 90f);
+        }
+
+        transform.eulerAngles = new Vector3(-mouseY, mouseX, 0);
+        transform.position = pivot.position - transform.forward * cameraDistance;
+    }
+
+}
